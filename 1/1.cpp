14177b588a3136670d3510f50316097a51b88cf6
Artur Lomako <artifaqiq@gmail.com>
2017-02-08 01:16:30 +0300

    lab1

--- 1/1.cpp
+++ 1/1.cpp
@@ -1,68 +1,153 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
-const int MATRIX_SIZE = 5;
-const int MATRIX_RAND_MAX = 5.0;
 
 #define M 6
 #define N 6
 #define MEM_ALIGN 16
 
-float** matrix_alloc();
-void randomize_matrix(float** matrix);
-void print_matrix(float** matrix);
+float** matrix2D_alloc();
+void randomize_matrix2D(float **matrix);
+void print_matrix2D(float **matrix);
+float**** matrix4D_alloc();
+void randomize_matrix4D(float ****matrix);
+void print_matrix4D(float ****matrix);
+float** matrix2D_sum(float** a, float** b);
+float** matrix2D_mul(float** a, float** b);
+float**** matrix4D_mul(float**** a, float **** b, int count);
 
-__attribute__((optimize("no-tree-vectorize")))
 int main()
 {
-    float** a = matrix_alloc(); randomize_matrix(a);
-    float** b = matrix_alloc(); randomize_matrix(b);
-    float** matrix_result = matrix_alloc();
+    float**** a = matrix4D_alloc();
+    randomize_matrix4D(a);
+    float**** b = matrix4D_alloc();
+    randomize_matrix4D(b);
+//    printf(" === A: \n"); print_matrix4D(a);
+//    printf(" === B: \n"); print_matrix4D(b);
 
-    for(int k = 0; k < 100000; k++) {
-
-        for(int i = 0; i < M; i++) {
-            for(int j = 0; j < N; j++) {
-
-                float sum = 0.0;
-                for(int k = 0; k < N; k++) {
-                    sum += a[i][k] + b[k][j];
-                }
-
-                matrix_result[i][j] = sum;
-
-            }
-        }
-    }
+    float**** result = matrix4D_mul(a, b, 1000);
+//    printf(" === C: \n"); print_matrix4D(result);
 
     return 0;
 }
 
-float** matrix_alloc()
+float**** matrix4D_mul(float**** a, float **** b, int count)
 {
-    float** result = (float**)malloc(M * sizeof(float*));
+    float**** result = matrix4D_alloc();
+
+    for(int ct = 0; ct < count; ct++) {
+
+        for(int i = 0; i < M; i++) {
+            for(int j = 0; j < M; j++) {
+
+                float** sum = matrix2D_alloc();
+                for(int k = 0; k < M; k++) {
+                    sum = matrix2D_sum(sum, matrix2D_mul(a[i][k], b[k][j]));
+                }
+
+                result[i][j] = sum;
+            }
+        }
+    }
+
+    return result;
+
+}
+
+float** matrix2D_mul(float** a, float** b)
+{
+    float** result = matrix2D_alloc();
+    float sum;
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+
+            sum =
+                    a[i][0] * b[0][j]
+                    + a[i][1] * b[1][j]
+                    + a[i][2] * b[2][j]
+                    + a[i][3] * b[3][j]
+                    + a[i][4] * b[4][j]
+                    + a[i][5] * b[5][j];
+
+            result[i][j] = sum;
+        }
+    }
+
+    return result;
+
+}
+
+float** matrix2D_sum(float** a, float** b)
+{
+    float** result = matrix2D_alloc();
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            result[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    return result;
+}
+
+float**** matrix4D_alloc()
+{
+    float**** result = (float****)aligned_alloc(MEM_ALIGN, M * sizeof(float***));
     for(int i = 0; i < M; i++) {
+        result[i] = (float***)aligned_alloc(MEM_ALIGN, M * sizeof(float**));
+    }
+    for(int i = 0; i < M; i++) {
+        for(int j = 0; j < M; j++) {
+            result[i][j] = matrix2D_alloc();
+        }
+    }
+
+    return result;
+}
+
+void randomize_matrix4D(float**** matrix)
+{
+    for(int i = 0; i < M; i++) {
+        for(int j = 0; j < M; j++) {
+            randomize_matrix2D(matrix[i][j]);
+        }
+    }
+}
+
+void print_matrix4D(float****matrix)
+{
+    for(int i = 0; i < M; i++) {
+        for(int j = 0; j < M; j++) {
+            printf(" ::: [%d][%d]:\n", i, j);
+            print_matrix2D(matrix[i][j]);
+        }
+    }
+}
+
+float** matrix2D_alloc()
+{
+    float** result = (float**)aligned_alloc(MEM_ALIGN, N * sizeof(float*));
+    for(int i = 0; i < N; i++) {
         result[i] = (float*)aligned_alloc(MEM_ALIGN, N * sizeof(float));
     }
 
     return result;
 }
 
-void randomize_matrix(float** matrix)
+void randomize_matrix2D(float **matrix)
 {
-    for(int i = 0; i < M; i++) {
+    for(int i = 0; i < N; i++) {
         for(int j = 0; j < N; j++) {
             matrix[i][j] = i * 10 + j;
         }
     }
 }
 
-void print_matrix(float** matrix)
+void print_matrix2D(float **matrix)
 {
-    for(int i = 0; i < M; i++) {
+    for(int i = 0; i < N; i++) {
         for(int j = 0; j < N; j++) {
-            printf("%5.1f ", matrix[i][j]);
+            printf("%7.1f ", matrix[i][j]);
         }
         printf("\n");
     }
--- 1/2.cpp
+++ 1/2.cpp
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * GCC Vector built-in functions
  * https://gcc.gnu.org/onlinedocs/gcc-4.9.2/gcc/X86-Built-in-Functions.html
  * */
@@ -11,295 +11,358 @@
 #define N 6
 
 
+float** matrix2D_alloc();
+void randomize_matrix2D(float **matrix);
+void print_matrix2D(float **matrix);
+float**** matrix4D_alloc();
+void randomize_matrix4D(float ****matrix);
+void print_matrix4D(float ****matrix);
+float** matrix2D_sum(float** a, float** b);
+float** matrix2D_mul(float** a, float** b);
+float**** matrix4D_mul(float**** a, float **** b, int count);
 
-typedef float v4sf __attribute__ ((vector_size (MEM_ALIGN)));
-
-float** matrix_alloc();
-void randomize_matrix(float** matrix);
-void print_matrix(float** matrix);
-
-#define print_vf(v) printf("%5.1f %5.1f %5.1f %5.1f\n", v[0], v[1], v[2], v[3])
-#define memsetf(ptr, v) for(int _i = 0; _i < 4; _i++) {ptr[_i] = v;}
 
 int main() {
-    __builtin_cpu_init();
+    float**** a = matrix4D_alloc();
+    randomize_matrix4D(a);
+    float**** b = matrix4D_alloc();
+    randomize_matrix4D(b);
+//    printf(" === A: \n"); print_matrix4D(a);
+//    printf(" === B: \n"); print_matrix4D(b);
 
-    float** a = matrix_alloc(); randomize_matrix(a);
-    printf("A:\n"); print_matrix(a);
-    float** b = matrix_alloc(); randomize_matrix(b);
-    printf("B:\n"); print_matrix(b);
-    float** c = matrix_alloc();
-    float* temp = (float*)aligned_alloc(MEM_ALIGN, 4 * sizeof(float));
-
-    // c[6x4]
-    for(int i = 0; i < M; i++) {
-
-    	printf("temp = %p\n a = %p\n b = %p\n c = %p\n\n", temp, a[i], b, c[i]);
-        asm volatile
-        (
-        	// xmm0-xmm5 == b[6x4]
-            "movl (%%edx), %%eax;"
-            "movups (%%eax), %%xmm0;"
-            
-            "movl 8(%%edx), %%eax;"
-            "movups (%%eax), %%xmm1;"
-            
-            "movl 16(%%edx), %%eax;"
-            "movups (%%eax), %%xmm2;"
-            
-            "movl 24(%%edx), %%eax;"
-            "movups (%%eax), %%xmm3;"
-
-            "movl 32(%%edx), %%eax;"
-            "movups (%%eax), %%xmm4;"
-
-            "movl 40(%%edx), %%eax;"
-            "movups (%%eax), %%xmm5;"
-
-            //xmm0 * a[i][0]
-            "movl 0(%%ecx), %%eax;"
-            "movl %%eax, 0(%1);"
-            "movl %%eax, 4(%1);"
-            "movl %%eax, 8(%1);"
-            "movl %%eax, 12(%1);"
-
-            "movups (%1), %%xmm6;"
-            "mulps %%xmm6, %%xmm0;"
-
-            //xmm1 * a[i][1]
-            "movl 4(%%ecx), %%eax;"
-            "movl %%eax, 0(%1);"
-            "movl %%eax, 4(%1);"
-            "movl %%eax, 8(%1);"
-            "movl %%eax, 12(%1);"
-
-            "movups (%1), %%xmm6;"
-            "mulps %%xmm6, %%xmm1;"
-
-            //xmm2 * a[i][2]
-            "movl 8(%%ecx), %%eax;"
-            "movl %%eax, 0(%1);"
-            "movl %%eax, 4(%1);"
-            "movl %%eax, 8(%1);"
-            "movl %%eax, 12(%1);"
-
-            "movups (%1), %%xmm6;"
-            "mulps %%xmm6, %%xmm2;"
-
-            //xmm3 * a[i][3]
-            "movl 12(%%ecx), %%eax;"
-            "movl %%eax, 0(%1);"
-            "movl %%eax, 4(%1);"
-            "movl %%eax, 8(%1);"
-            "movl %%eax, 12(%1);"
-
-            "movups (%1), %%xmm6;"
-            "mulps %%xmm6, %%xmm3;"
-
-            //xmm4 * a[i][4]
-            "movl 16(%%ecx), %%eax;"
-            "movl %%eax, 0(%1);"
-            "movl %%eax, 4(%1);"
-            "movl %%eax, 8(%1);"
-            "movl %%eax, 12(%1);"
-
-            "movups (%1), %%xmm6;"
-            "mulps %%xmm6, %%xmm4;"
-
-            //xmm5 * a[i][5]
-            "movl 20(%%ecx), %%eax;"
-            "movl %%eax, 0(%1);"
-            "movl %%eax, 4(%1);"
-            "movl %%eax, 8(%1);"
-            "movl %%eax, 12(%1);"
-
-            "movups (%1), %%xmm6;"
-            "mulps %%xmm6, %%xmm5;"
-
-            //sum (xmm0 - xmm1) == c[i]
-            "addps %%xmm5, %%xmm4;"
-            "addps %%xmm4, %%xmm3;"
-            "addps %%xmm3, %%xmm2;"
-            "addps %%xmm2, %%xmm1;"
-            "addps %%xmm1, %%xmm0;"
- 
- 			"movups %%xmm0, (%0);"
-            :
-            :
-            "r"(c[i]),
-            "r"(temp),
-            "c"(a[i]),
-            "d"(b)
-            :
-            "%eax",
-            "%ebx",
-            "%xmm0",
-            "%xmm1",
-            "%xmm2",
-            "%xmm3",
-            "%xmm4",
-            "%xmm5",
-            "%xmm6"
-
-        );
-        printf("temp = %p\n a = %p\n b = %p\n c = %p\n\n", temp, a[i], b, c[i]);
-    }
-
-    // c[6x2]
-    for(int i = 0; i < M; i++) {
-
-    	printf("temp = %p\n a = %p\n b = %p\n c = %p\n\n", temp, a[i], b, c[i]);
-        asm volatile
-        (
-        	// xmm0-xmm5 == b[6x2]
-            "movl 0(%%edx), %%eax;"
-            "movhps 16(%%eax), %%xmm0;"
-            
-            "movl 8(%%edx), %%eax;"
-            "movhps 16(%%eax), %%xmm1;"
-            
-            "movl 16(%%edx), %%eax;"
-            "movhps 16(%%eax), %%xmm2;"
-            
-            "movl 24(%%edx), %%eax;"
-            "movhps 16(%%eax), %%xmm3;"
-
-            "movl 32(%%edx), %%eax;"
-            "movhps 16(%%eax), %%xmm4;"
-
-            "movl 40(%%edx), %%eax;"
-            "movhps 16(%%eax), %%xmm5;"
-
-            //xmm0 * a[i][0]
-            "movl 0(%%ecx), %%eax;"
-            "movl %%eax, 0(%1);"
-            "movl %%eax, 4(%1);"
-            "movl %%eax, 8(%1);"
-            "movl %%eax, 12(%1);"
-
-            "movups (%1), %%xmm6;"
-            "mulps %%xmm6, %%xmm0;"
-
-            //xmm1 * a[i][1]
-            "movl 4(%%ecx), %%eax;"
-            "movl %%eax, 0(%1);"
-            "movl %%eax, 4(%1);"
-            "movl %%eax, 8(%1);"
-            "movl %%eax, 12(%1);"
-
-            "movups (%1), %%xmm6;"
-            "mulps %%xmm6, %%xmm1;"
-
-            //xmm2 * a[i][2]
-            "movl 8(%%ecx), %%eax;"
-            "movl %%eax, 0(%1);"
-            "movl %%eax, 4(%1);"
-            "movl %%eax, 8(%1);"
-            "movl %%eax, 12(%1);"
-
-            "movups (%1), %%xmm6;"
-            "mulps %%xmm6, %%xmm2;"
-
-            //xmm3 * a[i][3]
-            "movl 12(%%ecx), %%eax;"
-            "movl %%eax, 0(%1);"
-            "movl %%eax, 4(%1);"
-            "movl %%eax, 8(%1);"
-            "movl %%eax, 12(%1);"
-
-            "movups (%1), %%xmm6;"
-            "mulps %%xmm6, %%xmm3;"
-
-            //xmm4 * a[i][4]
-            "movl 16(%%ecx), %%eax;"
-            "movl %%eax, 0(%1);"
-            "movl %%eax, 4(%1);"
-            "movl %%eax, 8(%1);"
-            "movl %%eax, 12(%1);"
-
-            "movups (%1), %%xmm6;"
-            "mulps %%xmm6, %%xmm4;"
-
-            //xmm5 * a[i][5]
-            "movl 20(%%ecx), %%eax;"
-            "movl %%eax, 0(%1);"
-            "movl %%eax, 4(%1);"
-            "movl %%eax, 8(%1);"
-            "movl %%eax, 12(%1);"
-
-            "movups (%1), %%xmm6;"
-            "mulps %%xmm6, %%xmm5;"
-
-            //sum (xmm0 - xmm1) == c[i]
-            "addps %%xmm5, %%xmm4;"
-            "addps %%xmm4, %%xmm3;"
-            "addps %%xmm3, %%xmm2;"
-            "addps %%xmm2, %%xmm1;"
-            "addps %%xmm1, %%xmm0;"
- 
- 			"movhps %%xmm0, (%0);"
-            :
-            :
-            "r"(c[i] + 4),
-            "r"(temp),
-            "c"(a[i]),
-            "d"(b)
-            :
-            "%eax",
-            "%ebx",
-            "%xmm0",
-            "%xmm1",
-            "%xmm2",
-            "%xmm3",
-            "%xmm4",
-            "%xmm5",
-            "%xmm6"
-
-        );
-        printf("temp = %p\n a = %p\n b = %p\n c = %p\n\n", temp, a[i], b, c[i]);
-    }
-
-
-    printf("C:\n");
-    print_matrix(c);
-
-
-
-
-
-
-
-
+    float**** result = matrix4D_mul(a, b, 1000);
+//    printf(" === C: \n"); print_matrix4D(result);
 
     return 0;
 }
 
-float** matrix_alloc()
+float** matrix2D_mul(float** a, float** b)
 {
-    float** result = (float**)aligned_alloc(MEM_ALIGN, M * sizeof(float*));
+    float** c = matrix2D_alloc();
+    float* temp = (float*)aligned_alloc(MEM_ALIGN, 4 * sizeof(float));
+
+    for (int i = 0; i < N; i++) {
+
+        asm volatile
+        (
+        // xmm0-xmm5 == b[6x4]
+                "movl (%%edx), %%eax;"
+                "movups (%%eax), %%xmm0;"
+
+                "movl 8(%%edx), %%eax;"
+                "movups (%%eax), %%xmm1;"
+
+                "movl 16(%%edx), %%eax;"
+                "movups (%%eax), %%xmm2;"
+
+                "movl 24(%%edx), %%eax;"
+                "movups (%%eax), %%xmm3;"
+
+                "movl 32(%%edx), %%eax;"
+                "movups (%%eax), %%xmm4;"
+
+                "movl 40(%%edx), %%eax;"
+                "movups (%%eax), %%xmm5;"
+
+                //xmm0 * a[i][0]
+                "movl 0(%%ecx), %%eax;"
+                "movl %%eax, 0(%1);"
+                "movl %%eax, 4(%1);"
+                "movl %%eax, 8(%1);"
+                "movl %%eax, 12(%1);"
+
+                "movups (%1), %%xmm6;"
+                "mulps %%xmm6, %%xmm0;"
+
+                //xmm1 * a[i][1]
+                "movl 4(%%ecx), %%eax;"
+                "movl %%eax, 0(%1);"
+                "movl %%eax, 4(%1);"
+                "movl %%eax, 8(%1);"
+                "movl %%eax, 12(%1);"
+
+                "movups (%1), %%xmm6;"
+                "mulps %%xmm6, %%xmm1;"
+
+                //xmm2 * a[i][2]
+                "movl 8(%%ecx), %%eax;"
+                "movl %%eax, 0(%1);"
+                "movl %%eax, 4(%1);"
+                "movl %%eax, 8(%1);"
+                "movl %%eax, 12(%1);"
+
+                "movups (%1), %%xmm6;"
+                "mulps %%xmm6, %%xmm2;"
+
+                //xmm3 * a[i][3]
+                "movl 12(%%ecx), %%eax;"
+                "movl %%eax, 0(%1);"
+                "movl %%eax, 4(%1);"
+                "movl %%eax, 8(%1);"
+                "movl %%eax, 12(%1);"
+
+                "movups (%1), %%xmm6;"
+                "mulps %%xmm6, %%xmm3;"
+
+                //xmm4 * a[i][4]
+                "movl 16(%%ecx), %%eax;"
+                "movl %%eax, 0(%1);"
+                "movl %%eax, 4(%1);"
+                "movl %%eax, 8(%1);"
+                "movl %%eax, 12(%1);"
+
+                "movups (%1), %%xmm6;"
+                "mulps %%xmm6, %%xmm4;"
+
+                //xmm5 * a[i][5]
+                "movl 20(%%ecx), %%eax;"
+                "movl %%eax, 0(%1);"
+                "movl %%eax, 4(%1);"
+                "movl %%eax, 8(%1);"
+                "movl %%eax, 12(%1);"
+
+                "movups (%1), %%xmm6;"
+                "mulps %%xmm6, %%xmm5;"
+
+                //sum (xmm0 - xmm1) == c[i]
+                "addps %%xmm5, %%xmm4;"
+                "addps %%xmm4, %%xmm3;"
+                "addps %%xmm3, %%xmm2;"
+                "addps %%xmm2, %%xmm1;"
+                "addps %%xmm1, %%xmm0;"
+
+                "movups %%xmm0, (%0);"
+        :
+        :
+        "r"(c[i]),
+        "r"(temp),
+        "c"(a[i]),
+        "d"(b)
+        :
+        "%eax",
+        "%ebx",
+        "%xmm0",
+        "%xmm1",
+        "%xmm2",
+        "%xmm3",
+        "%xmm4",
+        "%xmm5",
+        "%xmm6"
+
+        );
+    }
+
+    // c[6x2]
+    for (int i = 0; i < M; i++) {
+
+        asm volatile
+        (
+        // xmm0-xmm5 == b[6x2]
+        "movl 0(%%edx), %%eax;"
+                "movhps 16(%%eax), %%xmm0;"
+
+                "movl 8(%%edx), %%eax;"
+                "movhps 16(%%eax), %%xmm1;"
+
+                "movl 16(%%edx), %%eax;"
+                "movhps 16(%%eax), %%xmm2;"
+
+                "movl 24(%%edx), %%eax;"
+                "movhps 16(%%eax), %%xmm3;"
+
+                "movl 32(%%edx), %%eax;"
+                "movhps 16(%%eax), %%xmm4;"
+
+                "movl 40(%%edx), %%eax;"
+                "movhps 16(%%eax), %%xmm5;"
+
+                //xmm0 * a[i][0]
+                "movl 0(%%ecx), %%eax;"
+                "movl %%eax, 0(%1);"
+                "movl %%eax, 4(%1);"
+                "movl %%eax, 8(%1);"
+                "movl %%eax, 12(%1);"
+
+                "movups (%1), %%xmm6;"
+                "mulps %%xmm6, %%xmm0;"
+
+                //xmm1 * a[i][1]
+                "movl 4(%%ecx), %%eax;"
+                "movl %%eax, 0(%1);"
+                "movl %%eax, 4(%1);"
+                "movl %%eax, 8(%1);"
+                "movl %%eax, 12(%1);"
+
+                "movups (%1), %%xmm6;"
+                "mulps %%xmm6, %%xmm1;"
+
+                //xmm2 * a[i][2]
+                "movl 8(%%ecx), %%eax;"
+                "movl %%eax, 0(%1);"
+                "movl %%eax, 4(%1);"
+                "movl %%eax, 8(%1);"
+                "movl %%eax, 12(%1);"
+
+                "movups (%1), %%xmm6;"
+                "mulps %%xmm6, %%xmm2;"
+
+                //xmm3 * a[i][3]
+                "movl 12(%%ecx), %%eax;"
+                "movl %%eax, 0(%1);"
+                "movl %%eax, 4(%1);"
+                "movl %%eax, 8(%1);"
+                "movl %%eax, 12(%1);"
+
+                "movups (%1), %%xmm6;"
+                "mulps %%xmm6, %%xmm3;"
+
+                //xmm4 * a[i][4]
+                "movl 16(%%ecx), %%eax;"
+                "movl %%eax, 0(%1);"
+                "movl %%eax, 4(%1);"
+                "movl %%eax, 8(%1);"
+                "movl %%eax, 12(%1);"
+
+                "movups (%1), %%xmm6;"
+                "mulps %%xmm6, %%xmm4;"
+
+                //xmm5 * a[i][5]
+                "movl 20(%%ecx), %%eax;"
+                "movl %%eax, 0(%1);"
+                "movl %%eax, 4(%1);"
+                "movl %%eax, 8(%1);"
+                "movl %%eax, 12(%1);"
+
+                "movups (%1), %%xmm6;"
+                "mulps %%xmm6, %%xmm5;"
+
+                //sum (xmm0 - xmm1) == c[i]
+                "addps %%xmm5, %%xmm4;"
+                "addps %%xmm4, %%xmm3;"
+                "addps %%xmm3, %%xmm2;"
+                "addps %%xmm2, %%xmm1;"
+                "addps %%xmm1, %%xmm0;"
+
+                "movhps %%xmm0, (%0);"
+        :
+        :
+        "r"(c[i] + 4),
+        "r"(temp),
+        "c"(a[i]),
+        "d"(b)
+        :
+        "%eax",
+        "%ebx",
+        "%xmm0",
+        "%xmm1",
+        "%xmm2",
+        "%xmm3",
+        "%xmm4",
+        "%xmm5",
+        "%xmm6"
+
+        );
+    }
+
+    return c;
+
+}
+
+float** matrix2D_sum(float** a, float** b)
+{
+    float** result = matrix2D_alloc();
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            result[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    return result;
+}
+
+float**** matrix4D_alloc()
+{
+    float**** result = (float****)aligned_alloc(MEM_ALIGN, M * sizeof(float***));
     for(int i = 0; i < M; i++) {
+        result[i] = (float***)aligned_alloc(MEM_ALIGN, M * sizeof(float**));
+    }
+    for(int i = 0; i < M; i++) {
+        for(int j = 0; j < M; j++) {
+            result[i][j] = matrix2D_alloc();
+        }
+    }
+
+    return result;
+}
+
+float**** matrix4D_mul(float**** a, float **** b, int count)
+{
+    float**** result = matrix4D_alloc();
+
+    for(int ct = 0; ct < count / 2; ct++) {
+
+        for(int i = 0; i < M; i++) {
+            for(int j = 0; j < M; j++) {
+
+                float** sum = matrix2D_alloc();
+                for(int k = 0; k < M; k++) {
+                    sum = matrix2D_sum(sum, matrix2D_mul(a[i][k], b[k][j]));
+                }
+
+                result[i][j] = sum;
+            }
+        }
+    }
+
+    return result;
+
+}
+
+void randomize_matrix4D(float**** matrix)
+{
+    for(int i = 0; i < M; i++) {
+        for(int j = 0; j < M; j++) {
+            randomize_matrix2D(matrix[i][j]);
+        }
+    }
+}
+
+void print_matrix4D(float****matrix)
+{
+    for(int i = 0; i < M; i++) {
+        for(int j = 0; j < M; j++) {
+            printf(" ::: [%d][%d]:\n", i, j);
+            print_matrix2D(matrix[i][j]);
+        }
+    }
+}
+
+float** matrix2D_alloc()
+{
+    float** result = (float**)aligned_alloc(MEM_ALIGN, N * sizeof(float*));
+    for(int i = 0; i < N; i++) {
         result[i] = (float*)aligned_alloc(MEM_ALIGN, N * sizeof(float));
     }
 
     return result;
 }
 
-void randomize_matrix(float** matrix)
+void randomize_matrix2D(float **matrix)
 {
-    for(int i = 0; i < M; i++) {
+    for(int i = 0; i < N; i++) {
         for(int j = 0; j < N; j++) {
             matrix[i][j] = i * 10 + j;
         }
     }
 }
 
-void print_matrix(float** matrix)
+void print_matrix2D(float **matrix)
 {
-    for(int i = 0; i < M; i++) {
+    for(int i = 0; i < N; i++) {
         for(int j = 0; j < N; j++) {
-            printf("%5.1f ", matrix[i][j]);
+            printf("%7.1f ", matrix[i][j]);
         }
         printf("\n");
     }
 }
-
